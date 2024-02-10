4fe1c28db815667a9b3397a1b2dbc35b3bd3c470
AFilippov <64398851+WebAFilippov@users.noreply.github.com>
2024-02-10 23:46:02 +0700

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000;
+const MONGO_DB = process.env.MONGO_DB;
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log("Server listened on port 3000")
-})
+
+mongoose.connection.on('error', err => {
+  console.log("Error MongoDB " + err);
+});
+
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+});
+
+mongoose.connect(MONGO_DB)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log('Error connection MongoDB: ' + err));
